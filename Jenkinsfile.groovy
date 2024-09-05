728f9b9622716d7a97cae9827ca57e7aefcc6372
VinojPrasath <s223839258@deakin.edu.au>
2024-09-05 12:55:29 +1000

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -39,14 +39,14 @@ pipeline {
             }
         }
     }
-    post {
-                always {
-                    emailext(
-                        to: 'vinoj.prasath23@gmail.com',
-                        subject: "Test Email Notification - ${currentBuild.currentResult}",
-                        body: "This is a test email. The build has ${currentBuild.currentResult}.",
-                        attachLog: true
-                    )
-                }
-            }
+   post {
+        always {
+            emailext(
+                to: 'vinoj.prasath23@gmail.com',
+                subject: "Build ${currentBuild.currentResult}: ${env.JOB_NAME} - ${env.BUILD_NUMBER}",
+                body: "The build ${env.BUILD_NUMBER} for job ${env.JOB_NAME} has ${currentBuild.currentResult}. \n\nCheck the details here: ${env.BUILD_URL} \n\nPlease find the attached build log.",
+                attachLog: true
+            )
         }
+    }
+}
