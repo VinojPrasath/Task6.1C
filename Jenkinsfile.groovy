304aa14c70d4dde0f8e4fa88ee61fc141ad0302e
VinojPrasath <s223839258@deakin.edu.au>
2024-09-05 23:11:45 +1000

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,25 +11,23 @@ pipeline {
             steps {
                 echo 'Running unit test with Junit'
                 echo 'Running integration test using TestNG'
+                // Simulate saving test results to log file
+                writeFile file: 'unit_test_results.log', text: 'Unit and Integration Test Logs'
             }
             post {
                 success {
-                    script {
-                        archiveArtifacts artifacts: 'unit_test_results.log'
-                    }
-                    mail to: 'vinoj.prasath23@gmail.com',
-                    subject: "Unit and Integration Testing - Success",
-                    body: "The Unit and Integration Testing stage has passed.",
-                    attachmentsPattern: 'unit_test_results.log'
+                    archiveArtifacts artifacts: 'unit_test_results.log'
+                    emailext to: 'vinoj.prasath23@gmail.com',
+                             subject: "Unit and Integration Testing - Success",
+                             body: "The Unit and Integration Testing stage has passed.",
+                             attachmentsPattern: 'unit_test_results.log'
                 }
                 failure {
-                    script {
-                        archiveArtifacts artifacts: 'unit_test_results.log'
-                    }
-                    mail to: 'vinoj.prasath23@gmail.com',
-                    subject: "Unit and Integration Testing - Failed",
-                    body: "The Unit and Integration Testing stage has failed.",
-                    attachmentsPattern: 'unit_test_results.log'
+                    archiveArtifacts artifacts: 'unit_test_results.log'
+                    emailext to: 'vinoj.prasath23@gmail.com',
+                             subject: "Unit and Integration Testing - Failed",
+                             body: "The Unit and Integration Testing stage has failed.",
+                             attachmentsPattern: 'unit_test_results.log'
                 }
             }
         }
@@ -41,31 +39,29 @@ pipeline {
         stage('Security Scan') {
             steps {
                 echo 'Perform Security Scan using OWASP ZAP'
+                // Simulate saving security scan results to log file
+                writeFile file: 'security_scan.log', text: 'Security Scan Logs'
             }
-           post {
+            post {
                 success {
-                    script {
-                        archiveArtifacts artifacts: 'security_scan.log'
-                    }
-                    mail to: 'vinoj.prasath23@gmail.com',
-                    subject: "Security Scan - Success",
-                    body: "The Security Scan stage has passed.",
-                    attachmentsPattern: 'security_scan.log'
+                    archiveArtifacts artifacts: 'security_scan.log'
+                    emailext to: 'vinoj.prasath23@gmail.com',
+                             subject: "Security Scan - Success",
+                             body: "The Security Scan stage has passed.",
+                             attachmentsPattern: 'security_scan.log'
                 }
                 failure {
-                    script {
-                        archiveArtifacts artifacts: 'security_scan.log'
-                    }
-                    mail to: 'vinoj.prasath23@gmail.com',
-                    subject: "Security Scan - Failed",
-                    body: "The Security Scan stage has failed.",
-                    attachmentsPattern: 'security_scan.log'
+                    archiveArtifacts artifacts: 'security_scan.log'
+                    emailext to: 'vinoj.prasath23@gmail.com',
+                             subject: "Security Scan - Failed",
+                             body: "The Security Scan stage has failed.",
+                             attachmentsPattern: 'security_scan.log'
                 }
-            } 
+            }
         }
         stage('Deploy to Staging') {
             steps {
-                echo 'Deploying using the staging server AWS E2C'
+                echo 'Deploying using the staging server AWS EC2'
             }
         }
         stage('Integration Test on Staging') {
@@ -75,15 +71,15 @@ pipeline {
         }
         stage('Deploy to Production') {
             steps {
-                echo 'Deploy to productionusing AWS E2C'
+                echo 'Deploying to production using AWS EC2'
             }
         }
     }
-   post {
+    post {
         always {
-            mail to: 'vinoj.prasath23@gmail.com',
-            subject: "Pipeline Success",
-            body:"All stages are running sucessfully"
+            emailext to: 'vinoj.prasath23@gmail.com',
+                     subject: "Pipeline Success",
+                     body: "All stages are running successfully"
         }
     }
 }
