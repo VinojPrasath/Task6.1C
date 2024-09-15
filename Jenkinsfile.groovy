795a264e24eec26f95a1c21da1fd9350eb04681a
VinojPrasath <s223839258@deakin.edu.au>
2024-09-15 21:28:49 +1000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,23 +9,25 @@ pipeline {
         }
         stage('Unit and Integration Testing') {
             steps {
-                echo 'Running unit test with Junit'
+                echo 'Running unit test with JUnit'
                 echo 'Running integration test using TestNG'
-                
             }
             post {
                 success {
                     archiveArtifacts artifacts: 'unit_test_results.log'
-                    mail to: 'vinoj.prasath23@gmail.com',
-                             subject: "Unit and Integration Testing - Success",
-                             body: "The Unit and Integration Testing stage has passed."            
+                    emailext subject: "Unit and Integration Testing - Success",
+                             body: "The Unit and Integration Testing stage has passed.",
+                             to: 'vinoj.prasath23@gmail.com',
+                             attachmentsPattern: 'unit_test_results.log',
+                             attachLog: true
                 }
                 failure {
                     archiveArtifacts artifacts: 'unit_test_results.log'
-                    mail to: 'vinoj.prasath23@gmail.com',
-                             subject: "Unit and Integration Testing - Failed",
-                             body: "The Unit and Integration Testing stage has failed."
-                             
+                    emailext subject: "Unit and Integration Testing - Failed",
+                             body: "The Unit and Integration Testing stage has failed.",
+                             to: 'vinoj.prasath23@gmail.com',
+                             attachmentsPattern: 'unit_test_results.log',
+                             attachLog: true
                 }
             }
         }
@@ -36,34 +38,35 @@ pipeline {
         }
         stage('Security Scan') {
             steps {
-                echo 'Perform Security Scan using OWASP ZAP'
-               
+                echo 'Performing Security Scan using OWASP ZAP'
             }
             post {
                 success {
                     archiveArtifacts artifacts: 'security_scan.log'
-                    mail to: 'vinoj.prasath23@gmail.com',
-                             subject: "Security Scan - Success",
-                             body: "The Security Scan stage has passed."
-                             
+                    emailext subject: "Security Scan - Success",
+                             body: "The Security Scan stage has passed.",
+                             to: 'vinoj.prasath23@gmail.com',
+                             attachmentsPattern: 'security_scan.log',
+                             attachLog: true
                 }
                 failure {
                     archiveArtifacts artifacts: 'security_scan.log'
-                    mail to: 'vinoj.prasath23@gmail.com',
-                             subject: "Security Scan - Failed",
-                             body: "The Security Scan stage has failed."
-                             
+                    emailext subject: "Security Scan - Failed",
+                             body: "The Security Scan stage has failed.",
+                             to: 'vinoj.prasath23@gmail.com',
+                             attachmentsPattern: 'security_scan.log',
+                             attachLog: true
                 }
             }
         }
         stage('Deploy to Staging') {
             steps {
-                echo 'Deploying using the staging server AWS EC2'
+                echo 'Deploying to the staging server on AWS EC2'
             }
         }
         stage('Integration Test on Staging') {
             steps {
-                echo 'Running integration test on Staging'
+                echo 'Running integration tests on Staging'
             }
         }
         stage('Deploy to Production') {
@@ -74,9 +77,10 @@ pipeline {
     }
     post {
         always {
-            mail to: 'vinoj.prasath23@gmail.com',
-                     subject: "Pipeline Success",
-                     body: "All stages are running successfully"
+            emailext subject: "Pipeline Success",
+                     body: "All stages are running successfully.",
+                     to: 'vinoj.prasath23@gmail.com',
+                     attachLog: true
         }
     }
 }
